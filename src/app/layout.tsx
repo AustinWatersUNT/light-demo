5b99e509aab5f342da67dc4a8c6904aa79380596
Austin Waters <austinwaters16@yahoo.com>
2025-07-17 01:06:14 -0500

    Remove Tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import Navbar from "../components/Navbar";
 import StoreProvider from "./StoreProvider";
 import { Stack } from "@mui/material";
 import MuiProvider from "./MuiProvider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Light Demo",
@@ -29,8 +18,8 @@ export default function RootLayout({
   return (
     <StoreProvider>
       <MuiProvider>
-        <html lang="en" style={{ height: '100vh' }}>
-            <body style={{ height: '100vh' }}>
+        <html lang="en" style={{ height: '100vh', padding: 'none' }}>
+            <body style={{ height: '100vh', margin: '0' }}>
               <Stack height="100vh">
                 <Navbar />
                 {children}
